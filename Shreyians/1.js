20a512bacacc79af599cbf7259358c8803477be4
HiM3 <krmehta1452@gmail.com>
2024-07-08 17:51:49 +0530

    first commit

---
+++ Shreyians/1.js
@@ -0,0 +1,14 @@
+var a;
+a=12;
+console.log(b); 
+var b=5;
+// refernce () [] {}
+// primitives 
+/*for(i=1;i<=10;i++){
+    console.log(i);
+}*/
+i=1;
+while(i<=20){
+    console.log(a);
+    a++;
+}
